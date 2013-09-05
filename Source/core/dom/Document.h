ed889c680196a07991114ff1eb76d568d0219e42
Yoav Weiss <yoav@yoav.ws>
2013-09-05 02:22:41 +0200

    Fixed style issues I didn't create

--- Source/core/dom/Document.h
+++ Source/core/dom/Document.h
@@ -458,15 +458,15 @@ public:
 
     void evaluateMediaQueryList();
 
-    // Never returns 0.
+    // Never returns 0
     FormController* formController();
     Vector<String> formElementsState() const;
     void setStateForNewFormElements(const Vector<String>&);
 
-    FrameView* view() const; // can be NULL
-    Frame* frame() const { return m_frame; } // can be NULL
-    Settings* settings() const; // can be NULL
-    Page* page() const; // can be NULL
+    FrameView* view() const; // can be null
+    Frame* frame() const { return m_frame; } // can be null
+    Settings* settings() const; // can be null
+    Page* page() const; // can be null
 
     float devicePixelRatio() const;
 
